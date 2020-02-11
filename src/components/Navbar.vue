93599c52b7ba569207fa36b63502fc3b80a7c0b6
Francis Badasu <nytro04@gmail.com>
2020-02-11 10:27:27 +0000

    added navbar and home dummy names

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-info">
+    <router-link class="navbar-brand" to="/">AFG</router-link>
+    <button
+      type="button"
+      class="navbar-toggler"
+      data-toggle="collapse"
+      data-target="#assignmentOne"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="assignmentOne">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/services" class="nav-link">Services</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="m-4 text-right">
+      <router-link to="/add-survey" class="btn btn-info"
+        >Add New Survey <i class="fas fa-plus"></i
+      ></router-link>
+    </div>
+
+    <div class="card card-body mb-3">
+      <ul class="list-group">
+        <li class="list-group-item text-center">Kofi Mole</li>
+      </ul>
+    </div>
+    <div class="card card-body mb-3">
+      <ul class="list-group">
+        <li class="list-group-item text-center">Kofi Mole</li>
+      </ul>
+    </div>
+    <div class="card card-body mb-3">
+      <ul class="list-group">
+        <li class="list-group-item text-center">Kofi Mole</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {};
   }
 };
 </script>
